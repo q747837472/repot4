290fa0d12e5499e077594874f17dfcaca18a99ab
lubiao <lubiao@qq.com>
2019-11-26 15:16:07 +0800

    第一次

---
+++ idea_git/src/com/kgc/hello.java
@@ -0,0 +1,11 @@
+package com.kgc;
+
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("commit");
+        System.out.println("commit");
+        System.out.println("commit");
+        System.out.println("commit");
+        System.out.println("commit");
+    }
+}
